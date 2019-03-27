9a4dea857eaa572ff3594af45b6c5f340c8ec700
JiHongYuan <1206429202@qq.com>
2019-03-27 22:41:38 +0800

    登出成功

---
+++ TestSpring/src/main/java/com/security/handler/MyLogoutSuccessHandler.java
@@ -0,0 +1,32 @@
+package com.security.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author jiHongYuan
+ * @Title: My
+ * @ProjectName springSecurityDemo
+ * @date 2019/3/2721:44
+ */
+@Component
+public class MyLogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler {
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_OK);
+        request.setCharacterEncoding("utf-8");
+        response.setHeader("Content-type", "text/html;charset=UTF-8");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+        out.write("{\"status\":\"success\",\"msg\":\"登出成功!\"}");
+        out.flush();
+        out.close();
+    }
+}
